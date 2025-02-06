195fd8b1ea7ad0cbed1ce380d9c9e7b76f7d2262
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-02-06 22:34:57 +0530

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider themes={["light", "dark", "pink"]} attribute="class">
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
