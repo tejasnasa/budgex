968445a10cba6c26b30c81a248fc0ecc4211bad7
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-03-27 14:44:29 +0530

    Create login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import logo from "@/assets/images/logo.png";
-import Link from "next/link";
 
 export default function page() {
   return (
     <main className={styles.main}>
       <Image src={logo} alt="Budgex" height={56} className={styles.logo} />
-      <section>
-        <h1>Login</h1>
-        <form action="">
-          <input type="text" />
-          <input type="text" />
-          <button>Submit</button>
-          <Link href="/register">Login</Link>
+      <section className={styles.box}>
+        <h1 className={styles.heading}>Login</h1>
+        <form action="" className={styles.form}>
+          <input type="text" className={styles.input} />
+          <input type="text" className={styles.input} />
+          <button className={styles.submit}>Submit</button>
         </form>
       </section>
     </main>
